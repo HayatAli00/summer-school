eec7728e1fb94dbbab5e1065b8f01dc84e910823
HayatAli00 <129970379+HayatAli00@users.noreply.github.com>
2023-07-19 18:47:32 +0100

    hg

--- app/routes.js
+++ app/routes.js
@@ -7,3 +7,31 @@ const govukPrototypeKit = require('govuk-prototype-kit')
 const router = govukPrototypeKit.requests.setupRouter()
 
 // Add your routes here
+
+//Branching on elgblity check page
+router.post('/eligblitycheck1', function (req, res) {
+
+  var abc = req.session.data['where-do-you-live']
+
+  if (abc == "M-academies"){
+
+    res.redirect('/introduction-page')
+
+  } else {
+
+    res.redirect('/not-eligible-page')
+
+  }
+
+})
+//  navigation from ukno to eligable 
+
+router.post('/ukno', function (req, res) {
+
+    {
+  
+      res.redirect('/eligblitycheck1')
+  
+    }
+  
+  })
